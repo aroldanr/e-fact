98126eb715149e303ac63914290afc65050f9718
Aldo <aldorm6000@gmail.com>
2021-01-31 20:50:08 -0600

    se grega formulario para visualizar facturas

---
+++ registrofacturasadmin.php
@@ -0,0 +1,80 @@
+<?php
+require_once 'header.php';
+$mode = isset($_REQUEST['f_mode']) ? $_REQUEST['f_mode'] : "";
+
+?>
+
+<html lang="en">
+
+<body>
+    <br><br>
+    <div class="container">
+        <h2>Registro de Facturas</h2>
+        <div class="panel-group">
+            <div class="panel panel-default">
+                <div class="panel-heading">Administracion</div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <table id="table_facturas" class="table table-bordered  display nowrap" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Factura No.</th>
+                                    <th>Fecha</th>
+                                    <th>Cliente</th>
+                                    <th>Vendedor</th>
+                                    <th>Estado</th>
+                                    <th>Accion</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+
+
+            var table = $("#table_facturas").DataTable({
+                pageLength: 10,
+
+                "ajax": {
+                    "method": "POST",
+                    "url": "registrodefacturasadmindata.php",
+
+                },
+                "columns": [{
+                        "data": "numero"
+                    },
+                    {
+                        "data": "fecha"
+                    },
+                    {
+                        "data": "nombre_cliente"
+                    },
+                    {
+                        "data": "nombre_vendedor"
+                    },
+                    {
+                        "data": "estado"
+                    },
+                    {
+                        "data": "estado"
+                    }
+                ]
+            });
+
+        });
+    </script>
+
+
+</body>
+
+<?php
+require_once 'footer.php';
+
+?>
+
+</html>
